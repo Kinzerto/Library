f4e0acd0d7c593b75d8c81e864b7553c91466e67
Kinth Enrique Cabuello <blood4249@gmail.com>
2025-12-05 00:20:43 +0800

    refactor: update Book constructor to include author and page, enhance library display with additional elements

--- script.js
+++ script.js
@@ -1,24 +1,66 @@
 const myLibrary = [];
 
-function Book(book) {
+function Book(book, author, page) {
     if (!new.target) {
         throw Error("You must use the 'new' operator to call the constructor");
     }
-    this.bookName = book
+    this.id = crypto.randomUUID();
+    this.bookName = book;
+    this.author = author;
+    this.page = page;
 }
 
 function addBookToLibrary(book) {
-    // take params, create a book then store it in the array
-    myLibrary.push(book)
+    myLibrary.push(book);
 }
 
-const book1 = new Book('One Piece');
-const book2 = new Book('Bleach');
+const book1 = new Book('one piece', 'echiro oda', 1167);
+const book2 = new Book('Bleach','Tite Kubo' , 686);
+
+
 
 
 addBookToLibrary(book1);
 addBookToLibrary(book2);
 
+const main = document.querySelector('main');
+
+myLibrary.forEach((books) => {
+    const cardContainer = document.createElement("div");
+    main.appendChild(cardContainer);
+    cardContainer.classList.add("card");
+
+    //CONTENT
+    const bookName = document.createElement('h2');
+    const author = document.createElement('div');
+    const page = document.createElement('div');
+
+    cardContainer.appendChild(bookName);
+    cardContainer.appendChild(author);
+    cardContainer.appendChild(page);
+
+    author.classList.add('author');
+    page.classList.add('pages');
+
+    // BUTTONS
+    const status = document.createElement('button');
+    const remove = document.createElement('button');
+
+    cardContainer.appendChild(status);
+    cardContainer.appendChild(remove);
+
+    status.classList.add('status');
+    remove.classList.add('remove');
+
+    // DOM
+    bookName.textContent = books.bookName;
+    author.textContent = books.author;
+    page.textContent = books.page;
+    status.textContent = 'mark as read';
+    remove.textContent = 'remove';
+
+    // cardContainer.textContent = books.bookName
+
+})
 
 
-console.log(myLibrary);
