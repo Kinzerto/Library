4ec61ff473cb0e44d943d9e47c69a7cddd128979
Kinth Enrique Cabuello <blood4249@gmail.com>
2025-12-07 12:14:17 +0800

    style: improve form handling and dialog interactions for book submission

--- script.js
+++ script.js
@@ -13,38 +13,57 @@ function Book(book, author, page) {
 function addBookToLibrary(book) {
     myLibrary.push(book);
 }
+const form = document.querySelector("dialog > form");
 
+// PREVENT FORM RELOAD
+form.addEventListener("submit", function (e) {
+    e.preventDefault(); // stops page from reloading
+    console.log("Form prevented!");
+});
 
 addBookToLibrary(new Book('dded', 'dede', 222))
 
 // DIALOG
 const openModal = document.querySelector('header  .open-modal');
 const dialog = document.querySelector(".dialog");
-const closeButton = document.querySelector("dialog button");
+const closeButton = document.querySelector("dialog > .close");
 
 
 openModal.addEventListener("click", () => {
     dialog.showModal();
 });
+closeButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    dialog.close();
+});
 
-const submit = document.querySelector(".dialog button");
+// closeButton.innerHTML = "dkdkd";
+const submit = document.querySelector(".dialog form .submit");
 
 const libraryBook = document.querySelector('dialog #book-name');
 const libraryAuthor = document.querySelector('dialog #author');
-const libraryPage = document.querySelector('dialog #page');
+const libraryPage = document.querySelector('dialog #page')
 
-const output = document.querySelector('output')
 submit.addEventListener('click', () => {
-    addBookToLibrary(new Book(libraryBook.value, libraryAuthor.value, libraryPage.value));
-    displayBooks()
 
+
+    if (libraryBook.value === '' || libraryAuthor.value === '' || isNaN(libraryPage.value) || libraryPage.value <= 0) {
+    } else {
+        const newBook = new Book(libraryBook.value, libraryAuthor.value, libraryPage.value);
+        addBookToLibrary(newBook);
+        displayBooks();
+        libraryBook.value = "";
+        libraryAuthor.value = "";
+        libraryPage.value = "";
+        dialog.close();
+    }
 })
 
 //DISPLAY BOOKS
 const main = document.querySelector('main');
 
 function displayBooks() {
-    main.textContent = ""; 
+    main.textContent = "";
     myLibrary.forEach((books) => {
         const cardContainer = document.createElement("div");
         main.appendChild(cardContainer);
